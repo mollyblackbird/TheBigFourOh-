66c8c6046842a6ccce30882f41831cd72cc3ed95
mollyblackbird <elsapatriciagg@gmail.com>
2026-07-16 15:13:08 +0100

    Update script.js
    Leave your birthday message!

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 
+🎉
+
+Happy 40th Birthday Patricia!
+
+Leave your birthday wishes below.
+
+-----------------------
+
+Name (optional)
+
+________________
+
+Message
+
+____________________
+
+____________________
+
+[ Send ❤️ ]
