27fe70cec86a1cde22a5274e4dd0f8f68b2f539f
mollyblackbird <elsapatriciagg@gmail.com>
2026-07-22 23:17:31 +0100

    Update script.js

--- script.js
+++ script.js
@@ -2,8 +2,38 @@ const supabaseUrl = "https://ozctyywmsukcsryhnsud.supabase.co/rest/v1/";
 
 const supabaseKey = "sb_publishable_zzN79OLLJ2aw37wdv9Ej5A_wNUA9GG1";
 
-const supabase =
-window.supabase.createClient(
-supabaseUrl,
-supabaseKey
+const form =
+document.getElementById("birthdayForm");
+
+form.addEventListener(
+"submit",
+
+async function(event){
+event.preventDefault();
+}
 );
+
+const { error } =
+await supabase
+
+.from("messages")
+
+.insert([
+{
+name: name,
+message: message
+}
+]);
+
+if (error) {
+
+alert("Sorry! Got too excited and something went wrong.");
+
+}
+
+else {
+
+alert("Thank you for your message!");
+  form.reset();
+
+}
