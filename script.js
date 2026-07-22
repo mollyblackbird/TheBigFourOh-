c6ea59edd4302f67ec7b0e9a99999740c9090504
mollyblackbird <elsapatriciagg@gmail.com>
2026-07-22 23:05:46 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
-await supabase
-  .from('messages')
-  .insert([
-    {
-      name: name,
-      message: message
-    }
-  ]);
+const supabaseUrl = "https://ozctyywmsukcsryhnsud.supabase.co/rest/v1/";
+
+const supabaseKey = "sb_publishable_zzN79OLLJ2aw37wdv9Ej5A_wNUA9GG1";
+
+const supabase =
+window.supabase.createClient(
+supabaseUrl,
+supabaseKey
+);
